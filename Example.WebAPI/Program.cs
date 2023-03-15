47a7cd12f1241a13d71a367fc3c2b59f08551bb1
Scott Vaughan <scott.vaughan@ensono.com>
2023-03-15 11:35:28 +0000

    Add project files.

---
+++ Example.WebAPI/Program.cs
@@ -0,0 +1,31 @@
+using Example.Application;
+using Example.Infrastructure;
+using Example.Presentation;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services
+    .AddApplication()
+    .AddInfrastructure()
+    .AddPresentation();
+
+builder.Host.UseSerilog((context, configuration) =>
+    configuration.ReadFrom.Configuration(context.Configuration));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseSerilogRequestLogging();
+
+app.UseHttpsRedirection();
+
+app.Run();
